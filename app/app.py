577145f69c618dc94acb08edb6bd848dc72bc278
Thomas59 <thomas.lemay59@outlook.fr>
2023-05-30 11:59:28 +0200

    Ajout de docstrings et refactoring du code

--- app/app.py
+++ app/app.py
@@ -2,66 +2,43 @@ import streamlit as st
 import pandas as pd
 import pickle
 
-# Charger le modèle depuis le fichier pickle
-model_rf = pickle.load(open('../models/model_carprice.pkl', 'rb'))
+def charger_donnees():
+    """Cette fonction charge les données du modèle et du fichier csv."""
+    model_rf = pickle.load(open('../models/model_carprice.pkl', 'rb'))
+    df = pd.read_csv('../data/carprice_clean.csv')
+    return model_rf, df
 
-# Charger les données du fichier CSV
-df = pd.read_csv('../data/carprice_clean.csv')
+def interface_utilisateur(df):
+    """Cette fonction crée l'interface utilisateur pour entrer les informations de la voiture."""
+    st.write("# Prédiction du prix d'une voiture")
 
-# Créer une liste des colonnes numériques et catégorielles
-numeric_cols = ['longueur_voiture(cm)', 'largeur_voiture(cm)', 'poids_vehicule(kg)', 'chevaux', 'consommation_ville(L/100km)', 'consommation_autoroute(L/100km)', 'taille_moteur']
-categorial_cols = ['carburant', 'turbo', 'type_vehicule', 'roues_motrices', 'emplacement_moteur', 'type_moteur', 'nombre_cylindres', 'systeme_carburant', 'marque', 'modele']
+    numeric_cols = ['longueur_voiture(cm)', 'largeur_voiture(cm)', 'poids_vehicule(kg)', 
+                    'chevaux', 'consommation_ville(L/100km)', 'consommation_autoroute(L/100km)', 
+                    'taille_moteur']
+    categorial_cols = ['carburant', 'turbo', 'type_vehicule', 'roues_motrices', 
+                       'emplacement_moteur', 'type_moteur', 'nombre_cylindres', 
+                       'systeme_carburant', 'marque', 'modele']
 
-# Créer une interface utilisateur pour saisir les informations de la voiture
-st.write("# Prédiction du prix d'une voiture")
+    data = {}
 
-longueur = st.slider('Longueur de la voiture (cm)', float(df['longueur_voiture(cm)'].min()), float(df['longueur_voiture(cm)'].max()), float(df['longueur_voiture(cm)'].mean()))
-largeur = st.slider('Largeur de la voiture (cm)', float(df['largeur_voiture(cm)'].min()), float(df['largeur_voiture(cm)'].max()), float(df['largeur_voiture(cm)'].mean()))
-poids = st.slider('Poids de la voiture (kg)', float(df['poids_vehicule(kg)'].min()), float(df['poids_vehicule(kg)'].max()), float(df['poids_vehicule(kg)'].mean()))
-chevaux = st.slider('Puissance du moteur (chevaux)', int(df['chevaux'].min()), int(df['chevaux'].max()), int(df['chevaux'].mean()))
-taille_moteur = st.slider('taille moteur', int(df['taille_moteur'].min()), int(df['taille_moteur'].max()), int(df['taille_moteur'].mean()))
-consommation_ville = st.slider('Consommation en ville (L/100km)', float(df['consommation_ville(L/100km)'].min()), float(df['consommation_ville(L/100km)'].max()), float(df['consommation_ville(L/100km)'].mean()))
-consommation_autoroute = st.slider('Consommation sur autoroute (L/100km)', float(df['consommation_autoroute(L/100km)'].min()), float(df['consommation_autoroute(L/100km)'].max()), float(df['consommation_autoroute(L/100km)'].mean()))
+    for col in numeric_cols:
+        data[col] = st.slider(f'{col}', float(df[col].min()), float(df[col].max()), float(df[col].mean()))
+        
+    for col in categorial_cols:
+        data[col] = st.selectbox(f'{col}', options=list(df[col].unique()))
 
-marque = st.selectbox('marque', options=list(df['marque'].unique()))
-modele = st.selectbox('modele', options=list(df['modele'].unique()))
-roues_motrices = st.selectbox('roues motrices', options=list(df['roues_motrices'].unique()))
-emplacement_moteur = st.selectbox('emplacement moteur', options=list(df['emplacement_moteur'].unique()))
-type_moteur = st.selectbox('Type de moteur', options=list(df['type_moteur'].unique()))
-nombre_cylindres = st.selectbox('nombre cylindres', options=list(df['nombre_cylindres'].unique()))
-systeme_carburant = st.selectbox('systeme carburant', options=list(df['systeme_carburant'].unique()))
-carburant = st.selectbox('Type de carburant', options=list(df['carburant'].unique()))
-turbo = st.selectbox('Turbo', options=list(df['turbo'].unique()))
-type_vehicule = st.selectbox('Type de véhicule', options=list(df['type_vehicule'].unique()))
+    return data
 
-# Ajouter un bouton pour prédire le prix de la voiture
-if st.button('Prédire le prix de la voiture'):
-    # Créer un dictionnaire avec les données de la voiture
-    data = {
-        'longueur_voiture(cm)': longueur,
-        'largeur_voiture(cm)': largeur,
-        'poids_vehicule(kg)': poids,
-        'chevaux': chevaux,
-        'taille_moteur' : taille_moteur,
-        'consommation_ville(L/100km)': consommation_ville,
-        'consommation_autoroute(L/100km)': consommation_autoroute,
-        'carburant': carburant,
-        'turbo': turbo,
-        'type_vehicule': type_vehicule,
-        'roues_motrices': roues_motrices,
-        'emplacement_moteur': emplacement_moteur,
-        'type_moteur': type_moteur,
-        'nombre_cylindres': nombre_cylindres,
-        'systeme_carburant': systeme_carburant,
-        'marque': marque,
-        'modele': modele
-    }
-
-    # Créer un dataframe à partir du dictionnaire de données
-    input_data = pd.DataFrame(data, index=[0])
-
-    # Faire la prédiction avec le modèle
-    prediction = model_rf.predict(input_data)
-
-    # Afficher le résultat de la prédiction
+def faire_prediction(model, input_data):
+    """Cette fonction utilise le modèle pour faire une prédiction basée sur les données d'entrée."""
+    prediction = model.predict(input_data)
     st.write(f"Le prix de la voiture est estimé à {prediction[0]:.2f} Dollars.")
+
+# Exécution principale
+if __name__ == "__main__":
+    model_rf, df = charger_donnees()
+    user_input = interface_utilisateur(df)
+    
+    if st.button('Prédire le prix de la voiture'):
+        input_data = pd.DataFrame(user_input, index=[0])
+        faire_prediction(model_rf, input_data)
